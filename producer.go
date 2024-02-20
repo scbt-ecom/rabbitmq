218680458e24951c05a4b2b900f3d4c7ae3f79ca
Trofimov Alexander Alekseevich <trofimovaa2@sovcombank.ru>
2024-02-20 10:53:32 +0300

    fix: channels multiplication

--- producer.go
+++ producer.go
@@ -1,31 +1,26 @@
 package rabbitmq
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"reflect"
 )
 
-// Produce message should be structure pointer
-func Produce(conn *amqp.Connection, message any, headers amqp.Table, exchange, key string) error {
+// ProduceWithContext message should be structure pointer
+func ProduceWithContext(ctx context.Context, ch *amqp.Channel, message any, headers amqp.Table, exchange, key string) error {
 	v := reflect.ValueOf(message)
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
 		return errors.New("message not a structure pointer")
 	}
-
-	ch, err := conn.Channel()
-	if err != nil {
-		return err
-	}
-	defer ch.Close()
 
 	body, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
 
-	err = ch.Publish(exchange, key, false, false, amqp.Publishing{
+	err = ch.PublishWithContext(ctx, exchange, key, false, false, amqp.Publishing{
 		ContentType: "application/json",
 		Body:        body,
 		Headers:     headers,
