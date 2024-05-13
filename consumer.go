655cab8b379dd5bc8e056f4b4bdf9d89bd979ea9
Trofimov Alexander Alekseevich <trofimovaa2@sovcombank.ru>
2024-05-13 14:18:21 +0300

    fix: change from conn to ch in q.Consume()

--- consumer.go
+++ consumer.go
@@ -4,13 +4,7 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func (q *Queue) Consume(conn *amqp.Connection, consumerName string) (<-chan amqp.Delivery, error) {
-	ch, err := conn.Channel()
-	if err != nil {
-		ch.Close()
-		return nil, err
-	}
-
+func (q *Queue) Consume(ch *amqp.Channel, consumerName string) (<-chan amqp.Delivery, error) {
 	msgs, err := ch.Consume(q.Name, consumerName, false, false, false, false, nil)
 	if err != nil {
 		ch.Close()
