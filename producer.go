3640c98dd19163ceae67b7fd742715e0de840b88
aftamoff <ramilahtamov1@gmail.com>
2024-11-27 22:55:39 +0300

    fix: chan buffer 100

--- producer.go
+++ producer.go
@@ -12,48 +12,53 @@ import (
 
 type Headers amqp.Table
 
-// ProduceWithContext message should be structure pointer
 func ProduceWithContext(ctx context.Context, ch *amqp.Channel, message any, headers Headers, exchange, key string) error {
-    v := reflect.ValueOf(message)
+	v := reflect.ValueOf(message)
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
-		return errors.New("message not a structure pointer")
+		return errors.New("message must be a pointer to a structure")
 	}
-    body, err := json.Marshal(message)
+
+	body, err := json.Marshal(message)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal message: %w", err)
 	}
-    var wg sync.WaitGroup
-    wg.Add(1)
-    returnCh := make(chan amqp.Return, 1)
-    ch.NotifyReturn(returnCh)
-    ch.Confirm(false)
-    ackCh := ch.NotifyPublish(make(chan amqp.Confirmation, 1))
-    var publishErr error
-    var receivedReturn bool
-    go func() {
-        defer wg.Done()
-        select {
-        case ret := <-returnCh:
-            receivedReturn = true
-            publishErr = fmt.Errorf("failed to route message: %s", string(ret.Body))
-        case confirm := <-ackCh:
-            if !confirm.Ack {
-                publishErr = fmt.Errorf("message not acknowledged by broker")
-            }
-        case <-ctx.Done():
-            if !receivedReturn {
-                publishErr = fmt.Errorf("context deadline exceeded, but no return received")
-            }
-        }
-    }()
-    err = ch.PublishWithContext(ctx, exchange, key, true, false, amqp.Publishing{
+
+	// Включаем подтверждения доставки.
+	err = ch.Confirm(false)
+	if err != nil {
+		return fmt.Errorf("failed to set channel to confirm mode: %w", err)
+	}
+
+	ackCh := ch.NotifyPublish(make(chan amqp.Confirmation, 100))
+	returnCh := ch.NotifyReturn(make(chan amqp.Return, 100))
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	var publishErr error
+	go func() {
+		defer wg.Done()
+		select {
+		case ret := <-returnCh:
+			publishErr = fmt.Errorf("failed to route message: %s", string(ret.Body))
+		case confirm := <-ackCh:
+			if !confirm.Ack {
+				publishErr = fmt.Errorf("message not acknowledged by broker")
+			}
+		case <-ctx.Done():
+			publishErr = fmt.Errorf("context deadline exceeded: %w", ctx.Err())
+		}
+	}()
+
+	err = ch.PublishWithContext(ctx, exchange, key, true, false, amqp.Publishing{
 		ContentType: "application/json",
 		Body:        body,
 		Headers:     amqp.Table(headers),
-    })
-    if err != nil {
-        return fmt.Errorf("publish error: %w", err)
-    }
-    wg.Wait()
-    return publishErr
+	})
+	if err != nil {
+		return fmt.Errorf("publish error: %w", err)
+	}
+
+	wg.Wait()
+	return publishErr
 }
